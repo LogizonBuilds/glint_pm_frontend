4095850e39efeafe58a7d051289e77e8d1aaba2f
Ayobami6 <ayobamiele006@gmail.com>
2025-01-20 23:05:31 +0100

    update Footer component height from 508px to 408px

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Footer = (props: Props) => {
     return (
-        <footer className='left-0 w-full lg:h-[314px] h-[508px] bottom-0 mt-20 p-12 bg-other border-1'>
+        <footer className='left-0 w-full lg:h-[314px] h-[408px] bottom-0 mt-20 p-12 bg-other border-1'>
             <div className='grid grid-cols-2 lg:grid-cols-6 gap-4'>
                 <div>Logo</div>
                 <div>Contact</div>
