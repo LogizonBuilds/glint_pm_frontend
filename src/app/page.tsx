c687cbbc6fb06ed906185255ff4a81e491babcf5
Ayobami6 <ayobamiele006@gmail.com>
2025-01-21 00:07:32 +0100

    add Enquiry component and integrate it into Home; adjust Footer margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import Enquiry from "@/components/Enquiry";
 
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Enquiry />
     </>
   );
 }
