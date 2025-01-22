29a2642f1f05f67396af464f38f4d6b747130c34
Ayobami6 <ayobamiele006@gmail.com>
2025-01-22 20:22:57 +0100

    add WhoWeAre component and integrate it into Home; add new images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Hero from "@/components/Hero";
 import Enquiry from "@/components/Enquiry";
+import WhoWeAre from "@/components/WhoWeAre";
 
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <WhoWeAre />
       <Enquiry />
     </>
   );
