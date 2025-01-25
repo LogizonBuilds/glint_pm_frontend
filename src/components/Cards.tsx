7ec2431f52c3a3cd32ed59813434dfbc49ae4519
bigkid <egbeyemifisola777@gmail.com>
2025-01-25 22:32:03 +0100

    WhatWeDo

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 type Props = {
   heading: string;
@@ -17,7 +18,26 @@ const Cards = (props: Props) => {
         }}
         className="w-full h-72 border"
       ></div>
-      <div className="w-5/6 m-auto h-72 relative bottom-20 border bg-slate-50 rounded"></div>
+      <div className="w-[90%] m-auto h-64 relative bottom-20 bg-slate-50 pt-4 rounded">
+        <div className="h-[20%] px-6">
+          <h1 className="text-xl text-center text-[#717171]">
+            Transforming Ideas into Action
+          </h1>
+        </div>
+        <div className="h-[60%] px-4 py-6">
+          <p className="text-center text-sm text-[#717171]">
+            We combine cutting-edge technology with proven project management
+            strategies to help you achieve your goals effortlessly.{" "}
+          </p>
+        </div>
+        <div className="h-[20%] flex justify-center">
+          <p className="text-center font-bold text-[#FF0000]">Explore</p>
+          <FaLongArrowAltRight
+            color="#FF0000"
+            style={{ top: 4, position: "relative", left: 10 }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
