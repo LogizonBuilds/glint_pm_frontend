c130823353bc3b139e2a67bb6274c25f7bad4313
Ayobami6 <ayobamiele006@gmail.com>
2025-01-26 04:55:32 +0100

    refactor TestimonyCard component: improve layout for better content alignment

--- src/components/TestimonyCard.tsx
+++ src/components/TestimonyCard.tsx
@@ -10,13 +10,13 @@ type Props = {
 const TestimonyCard = ({ text, name, role }: Props) => {
     return (
         <div className='w-[387px] h-[250px] rounded-[16px] border-[1px] bg-[#EAF1FA]'>
-            <div className='flex flex-col justify-start p-4'>
+            <div className='flex flex-col h-full justify-start p-4'>
                 <Image src={"/images/quote.png"} alt='quote' width={30} height={20} />
                 <h1 className='mt-4'>
                     {text}
                 </h1>
 
-                <div className='flex flex-col bottom-0 top-0'>
+                <div className='flex flex-col justify-end h-full'>
                     <p className='font-bold'>
                         {name}
                     </p>
