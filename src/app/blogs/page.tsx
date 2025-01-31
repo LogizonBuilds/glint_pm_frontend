dbd857dbb37fc268866fc035a96c47abca745c4f
bigkid <egbeyemifisola777@gmail.com>
2025-01-31 23:38:42 +0100

    blog page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BlogHero from "@/components/BlogHero";
+import BlogContent from "@/components/BlogContents";
 
 const page = () => {
   return (
     <div>
       <BlogHero />
+      <BlogContent />
     </div>
   );
 };
---
+++ src/components/BlogContents.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Image from "next/image";
+import Button from "./Button";
+
+const BlogContent = () => {
+  const data = [
+    {
+      img: "/images/blogimg.png",
+      title: "Build Your API stack",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      img: "/images/blogimg.png",
+      title: "Build Your API stack",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      img: "/images/blogimg.png",
+      title: "Build Your API stack",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      img: "/images/blogimg.png",
+      title: "Build Your API stack",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      img: "/images/blogimg.png",
+      title: "Build Your API stack",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      img: "/images/blogimg.png",
+      title: "Build Your API stack",
+      content: "Lorem ipsum dolor sit amet consectetur.",
+    },
+  ];
+
+  return (
+    <div className="h-[175vh] pt-14 px-14 w-full">
+      <div className="h-3/4 w-full flex justify-around mt-10 flex-wrap">
+        {data.map((items, index) => (
+          <div
+            key={index}
+            className="group h-80 w-[30%] flex flex-col border rounded-md bg-[#F5F7FA] p-4"
+          >
+            <div className="h-[50%] relative flex justify-center">
+              <Image className="rounded-lg " src={items.img} alt="it" fill />
+            </div>
+            <div className="h-[40%] relative ">
+              <div className="w-full flex justify-between m-2">
+                <h1 className="text-left font-semibold text-[20px]">
+                  {items.title}
+                </h1>
+                <p className="m-1">10 Jan, 2024</p>
+              </div>
+              <p className="">{items.content}</p>
+            </div>
+            <div className="h-[10%] relative flex justify-center">
+              <Button
+                text="Read More"
+                textColor="#FF0000"
+                noBg
+                height={30}
+                weight={150}
+                icon
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogContent;
