23fcae0114eae0e178350d38706d520e1c65af8b
bigkid <egbeyemifisola777@gmail.com>
2025-01-31 02:52:27 +0100

    service list

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,59 @@
+"use client";
 import React from "react";
+import { IconType } from "react-icons/lib";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+interface iconProps {
+  Icon: IconType;
+  size?: number;
+  color?: string;
+}
 
 type Props = {
   height?: number;
   weight?: number;
   text: string;
   noBg?: boolean;
+  borderColor?: string;
+  textColor?: string;
+  icon?: boolean;
 };
 
-const Button = ({ weight, height, text, noBg }: Props) => {
+const IconWrapper: React.FC<iconProps> = ({ Icon, size, color }) => {
+  return <Icon size={size} color={color} />;
+};
+
+const Button = ({
+  weight,
+  height,
+  text,
+  noBg,
+  borderColor,
+  textColor,
+  icon,
+}: Props) => {
   return (
     <button
       style={{
         width: weight ? `${weight}px` : "158px",
         height: height ? `${height}px` : "42px",
+        borderColor: borderColor ? borderColor : "",
+        borderWidth: borderColor ? 1 : 0,
       }}
       onClick={() => console.log("Button clicked")} // will add functionality later
       className={
         noBg
-          ? `rounded-md flex justify-center cursor-pointer items-center`
-          : `bg-primary rounded-md flex justify-center cursor-pointer items-center`
+          ? `rounded-md flex cursor-pointer justify-around items-center`
+          : `bg-primary rounded-md flex justify-around cursor-pointer items-center`
       }
     >
-      <p className="text-gray-50 font-bold">{text}</p>
+      <p
+        style={{ color: textColor ? textColor : "#f9fafb" }}
+        className="font-bold"
+      >
+        {text}
+      </p>
+      {icon && <FaArrowRightLong color={textColor ? textColor : "#f9fafb"} />}
     </button>
   );
 };
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,19 +1,52 @@
 import React from "react";
+import Image from "next/image";
+import Button from "./Button";
 
 const ServicesBody = () => {
   return (
-    <div className="h-[120vh] p-14 border w-full">
+    <div className="h-[160vh] p-14 border w-full">
       <h1 className="font-medium text-3xl">
         We are <span className="text-red-600 font-bold">Dedicated</span> to{" "}
         <br /> serve you all time
       </h1>
-      <div className="h-3/4 w-full flex justify-between mt-10 flex-wrap">
-        <div className="h-[46%] w-[31%] border rounded-md bg-[#F5F7FA]"></div>
-        <div className="h-[46%] w-[31%] border rounded-md bg-[#F5F7FA]"></div>
-        <div className="h-[46%] w-[31%] border rounded-md bg-[#F5F7FA]"></div>
-        <div className="h-[46%] w-[31%] border rounded-md bg-[#F5F7FA]"></div>
-        <div className="h-[46%] w-[31%] border rounded-md bg-[#F5F7FA]"></div>
-        <div className="h-[46%] w-[31%] border rounded-md bg-[#F5F7FA]"></div>
+      <div className="h-3/4 w-full flex justify-around mt-10 flex-wrap">
+        <div className="group h-72 w-[31%] flex flex-col border rounded-md bg-[#F5F7FA] p-4">
+          <div className="h-[30%] flex justify-center">
+            <Image
+              className="rounded-lg"
+              src={"/images/it.png"}
+              alt="it"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div className="h-[60%] ">
+            <h1 className="text-center m-4 font-semibold text-[20px]">
+              IT Infrastructure Management
+            </h1>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet consectetur. Integer egestas congue eu
+              vel sollicitudin ipsum sed.{" "}
+            </p>
+          </div>
+          <div className="h-[10%] hidden justify-between group-hover:flex">
+            <Button text="More" height={30} weight={102} icon />
+            <Button
+              text="Purchase"
+              noBg
+              height={30}
+              weight={102}
+              borderColor={"#FF0000"}
+              textColor={"#FF0000"}
+              // icon={FaArrowRightLong}
+            />
+          </div>
+        </div>
+        <div className="h-72 w-[31%] border rounded-md bg-[#F5F7FA]"></div>
+        <div className="h-72 w-[31%] border rounded-md bg-[#F5F7FA]"></div>
+        <div className="h-72 w-[31%] border rounded-md bg-[#F5F7FA]"></div>
+        <div className="h-72 w-[31%] border rounded-md bg-[#F5F7FA]"></div>
+        <div className="h-72 w-[31%] border rounded-md bg-[#F5F7FA]"></div>
       </div>
     </div>
   );
