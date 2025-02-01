46649aaa1f6a90302e46865cb608197faf8e9c34
Ayobami6 <ayobamidele006@gmail.com>
2025-02-01 04:43:20 +0100

    Adds blog details page with BlogHero and BlogDetailsContent components

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,10 +1,34 @@
+"use client"
 import React from 'react'
+import BlogHero from '@/components/BlogHero'
+import { useParams } from 'next/navigation'
+import { BlogData } from '@/components/BlogDetailsContent'
+import BlogDetailsContent from '@/components/BlogDetailsContent'
 
-type Props = {}
 
-const BlogsDetail = (props: Props) => {
+
+const BlogsDetail = () => {
+  const {id} = useParams()
+  console.log(id)
+  const blog: BlogData = {
+    id: 1,
+    title: 'Blog 1',
+    content: 'This is the content of blog 1',
+    author: 'Author 1',
+    date: '2021-01-01',
+    image: '/images/blogimg.png"',
+  }
+
+  const blogs = [blog, blog, blog]
+
   return (
-    <div>BlogsDetail</div>
+    <>
+    <BlogHero/>
+    <div className='mt-6 p-8'>
+      <BlogDetailsContent data={blog} blogs={blogs} />
+
+    </div>
+    </>
   )
 }
 
