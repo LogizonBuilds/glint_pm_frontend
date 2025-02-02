f40fcf3f41eba3ee4ca1f0f9f39e22d3ae178d0e
Ayobami6 <ayobamidele006@gmail.com>
2025-02-02 21:47:23 +0100

    Refactor layout and styling in Appointment component for improved responsiveness and structure

--- src/components/Appointment.tsx
+++ src/components/Appointment.tsx
@@ -5,25 +5,24 @@ import { FaWhatsapp } from "react-icons/fa";
 const Appointment = () => {
   return (
     <div className="mt-16">
-      <div className="h-[30vh] bg-[#FF0000] rounded-lg w-[75%] mx-auto mb-28 flex">
-        <div className="w-1/12 flex justify-center items-center">
-          <div className="w-[50px] h-[50px] rounded-[25px] bg-gray-50 flex justify-center items-center">
+      <div className="sm:h-[30vh] h-[40vh]  bg-[#FF0000] rounded-lg w-[90%] sm:w-[75%] mx-auto mb-28 flex flex-col sm:flex-row">
+        <div className="w-full sm:w-1/12 flex justify-center items-center">
+          <div className="w-[50px] h-[50px] sm:mt-0 mt-6 rounded-[25px] bg-gray-50 flex justify-center items-center">
             <FaWhatsapp size={30} color="#1FAF38" />
           </div>
         </div>
-        <div className="w-3/12 flex flex-col justify-center">
+        <div className="w-full sm:w-3/12 flex flex-col justify-center items-center  p-4">
           <p className="text-left text-gray-50">Get a free Consultation</p>
-          <p className="text-left text-[#F5F7FA] font-bold text-[20px]">
+          <p className="text-left text-[#F5F7FA] font-bold text-[16px] sm:text-[20px]">
             +234 8142679648
           </p>
         </div>
-        <div className="w-4/12 flex justify-center items-center">
-          <p className="text-gray-50 font-medium text-[15px]">
-            Lorem ipsum dolor sit amet consectetur. Integer egestas congue eu
-            vel sollicitudin ipsum sed.{" "}
+        <div className="w-full sm:w-4/12 flex justify-center items-center p-4">
+          <p className="text-gray-50 font-medium text-[14px] sm:text-[15px] text-center">
+            Lorem ipsum dolor sit amet consectetur. Integer egestas congue eu vel sollicitudin ipsum sed.
           </p>
         </div>
-        <div className="w-4/12 flex justify-center items-center">
+        <div className="w-full sm:w-4/12 flex justify-center items-center p-4">
           <Button
             bg="#FFFFFF"
             text="Make Appointment"
@@ -34,6 +33,7 @@ const Appointment = () => {
         </div>
       </div>
     </div>
+
   );
 };
 
